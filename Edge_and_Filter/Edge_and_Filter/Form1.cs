251f0f2452cf4c994f87c2b129215f422042016e
Tink711 <quentin.remion@students.hevs.ch>
2018-12-06 16:00:41 +0100

    adjust add project

--- Edge_and_Filter/Edge_and_Filter/Form1.cs
+++ Edge_and_Filter/Edge_and_Filter/Form1.cs
@@ -16,5 +16,10 @@ namespace Edge_and_Filter
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }

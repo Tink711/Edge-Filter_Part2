a468ad5cdf4bf4dd9712b57b0ae0bbaed66e899c
Tink711 <quentin.remion@students.hevs.ch>
2018-12-28 18:59:52 +0100

    add separation

--- Edge_and_Filter/EdgeAndFilterTest/InputOutputTest.cs
+++ Edge_and_Filter/EdgeAndFilterTest/InputOutputTest.cs
@@ -39,27 +39,10 @@ namespace EdgeAndFilterTest
             }
         }
 
-        #region Attributs de tests supplémentaires
-        //
-        // Vous pouvez utiliser les attributs supplémentaires suivants lorsque vous écrivez vos tests :
-        //
-        // Utilisez ClassInitialize pour exécuter du code avant d'exécuter le premier test de la classe
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Utilisez ClassCleanup pour exécuter du code une fois que tous les tests d'une classe ont été exécutés
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Utilisez TestInitialize pour exécuter du code avant d'exécuter chaque test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Utilisez TestCleanup pour exécuter du code après que chaque test a été exécuté
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+        //==============================================================
+        //COPY TO SQUARE CANVAS
+        //==============================================================
+       
         //Test when sending a bitmap with superior width that canvas and height
         [TestMethod]
         public void TestCopyToSquareCanvasWithWidthGreaterThatCanvasAndHeight()

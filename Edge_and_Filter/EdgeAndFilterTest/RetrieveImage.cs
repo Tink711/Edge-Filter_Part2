e08d6e40da9cd2e4ce9fe729bcfff690ac0fa1bc
Jaufnet6 <jaufray.sornette@gmail.com>
2018-12-29 18:15:20 +0100

    Retrieve Image

--- Edge_and_Filter/EdgeAndFilterTest/RetrieveImage.cs
+++ Edge_and_Filter/EdgeAndFilterTest/RetrieveImage.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EdgeAndFilterTest
 {
-    class RetrieveImage
+    public class RetrieveImage
     {
+        public static Bitmap RetrieveImageFromPath(string filename)
+        {
+            StreamReader streamReader = new StreamReader(filename);
+            return (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+        }
     }
 }
